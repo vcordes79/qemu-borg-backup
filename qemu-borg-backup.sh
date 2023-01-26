21acdce66b7df1db8a538e29823c9f2b9744418a
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 <vc@fdatek.de>
2023-01-26 15:28:29 +0100

    guestinfo

--- qemu-borg-backup.sh
+++ qemu-borg-backup.sh
@@ -13,7 +13,7 @@ create_snapshot() {
     domain=$1; shift; ds=$*
     d=$(echo $domain | tr "[:upper:]" "[:lower:]")
     params="--no-metadata --atomic --disk-only"
-    if [ "$d" != "pfsense" ]; then
+    if virsh guestinfo $d >/dev/null; then
         params="--quiesce $params"
     fi
     if ! virsh snapshot-create-as --domain $domain --name borg.qcow2 $params $ds; then
