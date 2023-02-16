cd23f7dd3d6a89f335340c30bc9b3c8b86c33c0f
Volker Cordes <github@vcordes.info>
2023-02-16 07:50:01 +0100

    fix

--- borg-backup.sh
+++ borg-backup.sh
@@ -244,6 +244,7 @@ for v in $(env |grep BORG_DIRS); do
   v=`echo $v | cut -d\_ -f3`
   repo=`echo $v | cut -d\= -f1`
   dirs=`echo $v | cut -d\= -f2`
+  write_info ""$phase": <pre>borg create -v -C zstd --stats $BORG_EXCLUDE $BORG_REPO::$repo-'{now}' $dirs</pre>"
   result=`borg create -v -C zstd --stats $BORG_EXCLUDE $BORG_REPO::$repo-'{now}' $dirs 2>&1`
   exitCode=$?
   result="<pre>$result</pre>"
