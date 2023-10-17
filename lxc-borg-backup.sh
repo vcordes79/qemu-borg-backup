d44a86f65204180f97a177e94139f570378b61ba
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 <vc@fdatek.de>
2023-10-17 09:20:50 +0200

    fix

--- lxc-borg-backup.sh
+++ lxc-borg-backup.sh
@@ -10,22 +10,27 @@ if [ "x$LXC_STOP_MYSQL" != "x" ]; then
   lxc exec "$container" -- service mysql stop
 fi
 
+lxc_backup_dir="$LXC_STORAGE_PATH/containers-snapshots/$container/borgbackup"
+
 if ! lxc snapshot "$container" borgbackup; then
   echo "Error creating container snapshot"
+  lxc_backup_dir="$LXC_STORAGE_PATH/containers/$container/borgbackup"
   retval=1
 fi
 
 if [ "x$LXC_STOP_MYSQL" != "x" ]; then
   lxc exec "$container" -- service mysql start
 fi
-if ! borg create -v -C zstd --stats $BORG_EXCLUDE $BORG_REPO::$container-'{now}' "$LXC_STORAGE_PATH/containers-snapshots/$container/borgbackup" 2>&1; then
+if ! borg create -v -C zstd --stats $BORG_EXCLUDE $BORG_REPO::$container-'{now}' "$lxc_backup_dir" 2>&1; then
   echo "Error creating backup"
   retval=1
 fi
 
-if ! lxc delete "$container/borgbackup"; then
-  echo "Error removing snapshot"
-  retval=2
+if [ -d "$LXC_STORAGE_PATH/containers-snapshots/$container/borgbackup" ]; then
+  if ! lxc delete "$container/borgbackup"; then
+    echo "Error removing snapshot"
+    retval=1
+  fi
 fi
 
 exit $retval
