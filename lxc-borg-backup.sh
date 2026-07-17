79baec551e476180a7c99c724bfcc8461e50a20d
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 <vc@fdatek.de>
2026-07-17 14:25:42 +0200

    fixes storage path

--- lxc-borg-backup.sh
+++ lxc-borg-backup.sh
@@ -3,18 +3,24 @@ export PATH=/snap/bin:$PATH
 export container=$1
 retval=0
 
-if [ -d "$LXC_STORAGE_PATH/containers-snapshots/$container/borgbackup" ]; then
+export CONTAINER_STORAGE_PATH=$LXC_STORAGE_PATH
+POOL=$(lxc config device show $container | grep pool | cut -d : -f 2 | sed -z 's/^[[:space:]]*//' | sed -z 's/[[:space:]]*$//')
+if [ "x$POOL" != "x" ]; then 
+  export CONTAINER_STORAGE_PATH=`lxc storage get $POOL source`
+fi
+
+if [ -d "$CONTAINER_STORAGE_PATH/containers-snapshots/$container/borgbackup" ]; then
     lxc delete "$container/borgbackup"
 fi
 if [ "x$LXC_STOP_MYSQL" != "x" ]; then
   lxc exec "$container" -- service mysql stop
 fi
 
-lxc_backup_dir="$LXC_STORAGE_PATH/containers-snapshots/$container/borgbackup"
+lxc_backup_dir="$CONTAINER_STORAGE_PATH/containers-snapshots/$container/borgbackup"
 
 if ! lxc snapshot "$container" borgbackup; then
   echo "Error creating container snapshot"
-  lxc_backup_dir="$LXC_STORAGE_PATH/containers/$container"
+  lxc_backup_dir="$CONTAINER_STORAGE_PATH/containers/$container"
   retval=1
 fi
 
@@ -22,8 +28,8 @@ if [ "x$LXC_STOP_MYSQL" != "x" ]; then
   lxc exec "$container" -- service mysql start
 fi
 
-if [ -d "$LXC_STORAGE_PATH/containers-snapshots/$container/borgbackup" ]; then
-  cd "$LXC_STORAGE_PATH"
+if [ -d "$CONTAINER_STORAGE_PATH/containers-snapshots/$container/borgbackup" ]; then
+  cd "$CONTAINER_STORAGE_PATH"
   for i in $(btrfs subvolume list . |grep "containers-snapshots/$container/borgbackup" | grep "/.btrfs" | cut -d ' ' -f 9 | sort -r); do btrfs property set -ts "$i" ro false; done
   for i in $(btrfs subvolume list . |grep "containers-snapshots/$container/borgbackup" | grep "/.btrfs" | cut -d ' ' -f 9 | sort -r); do btrfs subvolume delete "$i"; done
   for i in $(btrfs subvolume list . |grep "containers-snapshots/$container/borgbackup" | grep "/.snapshots" | cut -d ' ' -f 9 | sort -r); do btrfs property set -ts "$i" ro false; done
@@ -46,12 +52,12 @@ while ! borg create -v -C zstd --stats $BORG_EXCLUDE $BORG_REPO::$container-'{no
   fi
 done
 
-if [ -d "$LXC_STORAGE_PATH/containers-snapshots/$container/borgbackup" ]; then
+if [ -d "$CONTAINER_STORAGE_PATH/containers-snapshots/$container/borgbackup" ]; then
   if ! lxc delete "$container/borgbackup"; then
-    cd "$LXC_STORAGE_PATH"
+    cd "$CONTAINER_STORAGE_PATH"
     for i in $(btrfs subvolume list . |grep "containers-snapshots/$container/borgbackup" | cut -d ' ' -f 9 | sort -r); do btrfs property set -ts "$i" ro false; done
     for i in $(btrfs subvolume list . |grep "containers-snapshots/$container/borgbackup" | cut -d ' ' -f 9 | sort -r); do btrfs subvolume delete "$i"; done
-    if [ -d "$LXC_STORAGE_PATH/containers-snapshots/$container/borgbackup" ]; then
+    if [ -d "$CONTAINER_STORAGE_PATH/containers-snapshots/$container/borgbackup" ]; then
       echo "Error removing snapshot"
       retval=2
     fi
